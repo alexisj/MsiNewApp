cb13320ad71fa3ea0ba1ec25b828e4b39413d27a
Alexis <alexisjoubert@groupemsi.com>
2011-09-09 13:00:11 -0400

    added fos user as submodule and msi user bundle

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Msi New App
 1. git submodule update --init
 2. php bin/vendors install
 3. ????
-4. profit!
+4. Profit!
--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Msi\UserBundle\MsiUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
